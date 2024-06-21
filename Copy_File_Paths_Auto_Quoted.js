618b4311e6f5f47ad97f486171f5e647336efcc7
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-06-21 10:32:33 -0700

    Update path auto copy script - add symlink resolving
    Adds variable and argument options to resolve symbolic link paths before copying to clipboard

--- Copy_File_Paths_Auto_Quoted.js
+++ Copy_File_Paths_Auto_Quoted.js
@@ -1,15 +1,15 @@
 // Copies files or folders full paths to clipboard with quotes around it if it has spaces. Various optional behavior for multiple selected items
 // By ThioJoe
-// Updated: 6/13/24
+// Updated: 6/21/24
 
 // Discussion Thread / Latest Version: https://resource.dopus.com/t/scripts-to-copy-file-folder-name-s-or-path-s-with-automatic-surrounding-quotes-based-on-spaces/51122
 // Updates also available on my GitHub repo: https://github.com/ThioJoe/D-Opus-Scripts
 
 //   Arguments Template (Must put this in the 'Template' box in the command editor to use arguments):
-//   MULTILINE_QUOTE_MODE/O,SINGLE_ITEM_QUOTE_MODE/O,FOLDER_TERMINATOR/O
+//   MULTILINE_QUOTE_MODE/O,SINGLE_ITEM_QUOTE_MODE/O,FOLDER_TERMINATOR/O,RESOLVE_SYMLINKS/O
 
 //   Example usage of arguments:
-//   Copy_File_Paths_Auto_Quoted MULTILINE_QUOTE_MODE="never" SINGLE_ITEM_QUOTE_MODE="auto" FOLDER_TERMINATOR="\"
+//   Copy_File_Paths_Auto_Quoted MULTILINE_QUOTE_MODE="never" SINGLE_ITEM_QUOTE_MODE="auto" FOLDER_TERMINATOR="\" RESOLVE_SYMLINKS="both"
 
 function OnClick(clickData) {
     //------------ Options (Note: If arguments are used when calling the script, these values will be overrided by the arguments) ------------
@@ -24,6 +24,11 @@ function OnClick(clickData) {
     // singleItemQuoteMode: Affects how quotes are added to each file name when a single file/folder is selected
     // >  Optional Argument Name: SINGLE_ITEM_QUOTE_MODE (string value)
     var singleItemQuoteMode = "auto";
+    // resolveSymlinksAndJunctions: Whether to copy the resulting linked target path instead of the 'virtual' file/folder for symbolic links (can be either files/folders) and junctions (folders).
+    // Note: Doesn't apply to shortcuts (.lnk files)
+    // Possible values: none, symlinkedFiles, symlinkedFolders, both
+    // >  Optional Argument Name: RESOLVE_SYMLINKS (string value)
+    var resolveSymlinks = "none";
     //---------------------------------
     
     var tab = clickData.func.sourcetab;
@@ -57,6 +62,18 @@ function OnClick(clickData) {
         //DOpus.Output("Received MULTILINE_QUOTE_MODE argument: " + String(clickData.func.args.MULTILINE_QUOTE_MODE));
     }
 
+    if (clickData.func.args.got_arg.RESOLVE_SYMLINKS) {
+        //Validate argument value
+        argString = clickData.func.args.RESOLVE_SYMLINKS.toLowerCase();
+        if (argString == "none" || argString == "symlinkedfiles" || argString == "symlinkedfolders" || argString == "both") {
+            resolveSymlinks = argString;
+        } else {
+            resolveSymlinks = "none";
+            DOpus.Output("ERROR: Invalid RESOLVE_SYMLINKS argument. Must be either 'none', 'symlinkedFiles', 'symlinkedFolders', or 'both'. Got: " + argString);
+        }
+        //DOpus.Output("Received RESOLVE_SYMLINKS argument: " + String(clickData.func.args.RESOLVE_SYMLINKS));
+    }
+
     if (clickData.func.args.got_arg.FOLDER_TERMINATOR) {
         includeTrailingTerminator = clickData.func.args.FOLDER_TERMINATOR;
         //DOpus.Output("Received FOLDER_TERMINATOR argument");
@@ -67,6 +84,13 @@ function OnClick(clickData) {
     if (selectedItems.count == 1) {
         var singleItem = selectedItems(0);
         var filePath = String(singleItem.realpath);
+        
+        // Resolve symlink/junction if needed
+        if (resolveSymlinks == "both" || (resolveSymlinks == "symlinkedfiles" && !singleItem.is_dir) || (resolveSymlinks == "symlinkedfolders" && singleItem.is_dir)) {
+            var resolvedPath = DOpus.FSUtil.Resolve(String(singleItem.realpath), "j");
+            filePath = String(resolvedPath);
+        }
+       
         if (singleItem.is_dir) {
             filePath += includeTrailingTerminator;
         }
@@ -81,6 +105,13 @@ function OnClick(clickData) {
         // Multiple items selected
         for (var i = 0; i < selectedItems.count; i++) {
             var filePath = String(selectedItems(i).realpath);
+
+            // Resolve symlink/junction if needed
+            if (resolveSymlinks == "both" || (resolveSymlinks == "symlinkedfiles" && !selectedItems(i).is_dir) || (resolveSymlinks == "symlinkedfolders" && selectedItems(i).is_dir)) {
+                var resolvedPath = DOpus.FSUtil.Resolve(String(selectedItems(i).realpath), "j");
+                filePath = String(resolvedPath);
+            }
+            
             if (selectedItems(i).is_dir) {
                 filePath += includeTrailingTerminator;
             }
