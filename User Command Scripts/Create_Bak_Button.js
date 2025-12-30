cca432cb6856bed80b18e4aad0b757094af14da9
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2025-12-29 17:56:16 -0700

    Add STEM_MODE to bak button command
    Add a STEM_MODE argument to allow inserting the backup extension before the file extension (e.g., File.bak.txt) instead of appending it (File.txt.bak)

--- User Command Scripts/Create_Bak_Button.js
+++ User Command Scripts/Create_Bak_Button.js
@@ -1,17 +1,18 @@
 // Button / User Command script that creates '.bak' backups for any number of selected files or folders. If a .bak already exists for an item, it will create .bak2, .bak3 and so on. Also has an argument option to restore a file.
 // By ThioJoe
-// Updated: 9/3/24
+// Updated: 12/29/25
 
 //    Argument Template:    
-//    RESTORE/S,RENAME/S,BACKUP_EXTENSION/K
+//    RESTORE/S,RENAME/S,BACKUP_EXTENSION/K,STEM_MODE/S
 
 //    Example usages of arguments:
 //       Make_bak BACKUP_EXTENSION=".bak"
+//       Make_bak STEM_MODE
 //       Make_bak RESTORE
 //       Make_bak BACKUP_EXTENSION=".backup" RESTORE
 
 function OnClick(clickData) {
-    // You can change the backup extension base string to be whatever you want here. Must include period.
+    // You can change the backup extension base string to be whatever you want here. Must include period. Can be overriden using the BACKUP_EXTENSION argument.
     // Default = '.bak'
     // >  Optional Argument Name: BACKUP_EXTENSION (string value)
     var backupExtension = '.bak';
@@ -26,6 +27,10 @@ function OnClick(clickData) {
     // >  Optional Argument Name: RENAME (Switch, no value needed)
     var doRename = false;
 
+    // With this set to true (or if argument is used), the backup extension is inserted before the file extension (e.g. File.bak.txt) instead of appended (File.txt.bak)
+    // >  Optional Argument Name: STEM_MODE (Switch, no value needed)
+    var doStemMode = false;
+
     // -----------------------------------------------------------------------
 
     // Parse optional arguments if they're there
@@ -37,6 +42,11 @@ function OnClick(clickData) {
     if (clickData.func.args.got_arg.RENAME) {
         doRename = true;
         //DOpus.Output("Received RENAME switch argument");
+    }
+
+    if (clickData.func.args.got_arg.STEM_MODE) {
+        doStemMode = true;
+        //DOpus.Output("Received STEM_MODE switch argument");
     }
     
     if (clickData.func.args.got_arg.BACKUP_EXTENSION) {
@@ -73,11 +83,25 @@ function OnClick(clickData) {
         
         // Construct command string depending on arguments
         var commandString;
-        if (doRename == true) {
-            // Renames to the new name. Added autonumber just in case
-            commandString = 'Rename FROM "' + selectedItem + '" TO *' + backupExtension + bakNumString + " AUTONUMBER";
+        if (doStemMode) {
+            // New logic for inserting extension in the middle
+            var itemStem = String(item.name_stem);
+            var itemExt = String(item.ext);
+            var newName = itemStem + backupExtension + bakNumString + itemExt;
+
+            if (doRename == true) {
+                commandString = 'Rename FROM "' + selectedItem + '" TO "' + newName + '"';
+            } else {
+                commandString = 'Copy DUPLICATE "' + selectedItem + '" AS "' + newName + '"';
+            }
         } else {
-            commandString = 'Copy DUPLICATE "' + selectedItem + '" AS *' + backupExtension + bakNumString;
+            // Original logic
+            if (doRename == true) {
+                // Renames to the new name. Added autonumber just in case
+                commandString = 'Rename FROM "' + selectedItem + '" TO *' + backupExtension + bakNumString + " AUTONUMBER";
+            } else {
+                commandString = 'Copy DUPLICATE "' + selectedItem + '" AS *' + backupExtension + bakNumString;
+            }
         }
         //DOpus.Output("Running command: " + commandString);
         clickData.func.command.RunCommand(commandString);
@@ -91,13 +115,30 @@ function OnClick(clickData) {
         // Get the full name of the selected .bak file
         var selectedBakFullName = String(item.name);
         
-        // Determine the base name of the original file by removing the .bak or .bak# extension
-        var originalFileName;
-        var baseNameRegex = new RegExp('^(.+)' + backupExtension.replace('.', '\\.') + '(\\d*)$');
-        //DOpus.Output("baseNameRegex:  " + baseNameRegex);
-        var match = selectedBakFullName.match(baseNameRegex);
-        if (match) {
-            originalFileName = match[1];
+        var originalFileName = null;
+        var escapedExt = backupExtension.replace('.', '\\.');
+
+        // 1. Try Standard Mode pattern (e.g. Test.txt.bak or Test.txt.bak2)
+        // Checks if file ends with extension + optional numbers
+        var standardRegex = new RegExp('^(.+)' + escapedExt + '(\\d*)$', 'i');
+        var standardMatch = selectedBakFullName.match(standardRegex);
+
+        if (standardMatch) {
+            originalFileName = standardMatch[1];
+        } else {
+            // 2. Try Stem Mode pattern (e.g. Test.bak.txt or Test.bak2.txt)
+            // Checks if extension + optional numbers is inside the filename, followed by an extension
+            var stemRegex = new RegExp('^(.+)' + escapedExt + '(\\d*)(\\..+)$', 'i');
+            var stemMatch = selectedBakFullName.match(stemRegex);
+            
+            if (stemMatch) {
+                // Reconstruct original name: Stem (Group 1) + Extension (Group 3)
+                originalFileName = stemMatch[1] + stemMatch[3];
+            }
+        }
+
+        if (originalFileName) {
+             // Valid match found, proceed
         } else {
             // Show error dialogue if the selected file is not a valid .bak file
             DOpus.Dlg.Request("Error: The selected file (" + selectedBakFullName + ") doesn't appear to match the selected backup extension: " + backupExtension, "OK");
@@ -154,33 +195,65 @@ function OnClick(clickData) {
 
             //DOpus.Output("Checking existing item: " + currentItemName);
 
-            // Checks if stem of currently scanned item is same as selected item with .bak added
-            var theoreticalBakName = selectedItemFullName + backupExtension;
-            var theoreticalBakNameLength = theoreticalBakName.length;
+            var extEnding = "";
+            var isMatch = false;
 
-            // Checking if the currently scanned item is already a .bak item of the selected item or folder
-            // By checking if scanned item contains selected item name + bak, from beginning
-            if (currentItemName.substr(0, theoreticalBakNameLength) == theoreticalBakName) {
-                //DOpus.Output("Found backup match: " + currentItemName);
+            if (doStemMode) {
+                var itemStem = String(item.name_stem);
+                var itemExt = String(item.ext);
+                var base = itemStem + backupExtension;
 
-                // Checks if extension is .bak or .bak*
-                if (currentItemName.length == theoreticalBakNameLength) {
-                    if (lastBakNum == 0) {
-                        lastBakNum = 1;
-                    }
-                    //DOpus.Output("Setting lastBakNum to 1");
-                } else {
-                    // Gets text or number after ".bak", which should be a number
-                    var extEnding = currentItemName.substr(theoreticalBakNameLength);
-                    // Checks if anything after .bak is not a number
-                    if (!isNaN(extEnding)) {
-                        // Parse the ending number into an integer in base 10
-                        var extEndingNum = parseInt(extEnding, 10);
-                        // Only update lastBakNum if it is the largest .bak # found so far
-                        if (extEndingNum > lastBakNum) {
-                            lastBakNum = extEndingNum;
-                            //DOpus.Output("Updating lastBakNum to: " + lastBakNum);
+                // Check if starts with stem+bak
+                if (currentItemName.indexOf(base) == 0) {
+                    // Check if ends with ext
+                    if (itemExt == "" || currentItemName.substr(currentItemName.length - itemExt.length) == itemExt) {
+                        // Extract the middle bit (number)
+                        var middlePart = currentItemName.substring(base.length, currentItemName.length - itemExt.length);
+                        
+                        // If the middle part is empty, it means we found the base backup (e.g. Test.bak.txt)
+                        if (middlePart == "") {
+                            if (lastBakNum == 0) {
+                                lastBakNum = 1;
+                            }
+                        } else {
+                            extEnding = middlePart;
+                            isMatch = true;
                         }
+                    }
+                }
+            } else {
+                // Checks if stem of currently scanned item is same as selected item with .bak added
+                var theoreticalBakName = selectedItemFullName + backupExtension;
+                var theoreticalBakNameLength = theoreticalBakName.length;
+
+                // Checking if the currently scanned item is already a .bak item of the selected item or folder
+                // By checking if scanned item contains selected item name + bak, from beginning
+                if (currentItemName.substr(0, theoreticalBakNameLength) == theoreticalBakName) {
+                    //DOpus.Output("Found backup match: " + currentItemName);
+
+                    // Checks if extension is .bak or .bak*
+                    if (currentItemName.length == theoreticalBakNameLength) {
+                        if (lastBakNum == 0) {
+                            lastBakNum = 1;
+                        }
+                        //DOpus.Output("Setting lastBakNum to 1");
+                    } else {
+                        // Gets text or number after ".bak", which should be a number
+                        extEnding = currentItemName.substr(theoreticalBakNameLength);
+                        isMatch = true;
+                    }
+                }
+            }
+
+            if (isMatch) {
+                // Checks if anything after .bak is not a number
+                if (extEnding != "" && !isNaN(extEnding)) {
+                    // Parse the ending number into an integer in base 10
+                    var extEndingNum = parseInt(extEnding, 10);
+                    // Only update lastBakNum if it is the largest .bak # found so far
+                    if (extEndingNum > lastBakNum) {
+                        lastBakNum = extEndingNum;
+                        //DOpus.Output("Updating lastBakNum to: " + lastBakNum);
                     }
                 }
             }
